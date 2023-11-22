f2a9067259aed3ff5746d40f89db38777ac3ba2f
Pushpraj Chaudhary <raftar4153@gmail.com>
2023-11-23 00:40:36 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+
+
+const PRIVATE_KEY = "be5bcb08c16826fe29fc93d9344f9135a604e18e779a1b479fdeea503e31406c";
+
+const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+      chainId: 80001,
+    },
+    polygon_mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
